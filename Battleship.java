926090cbd89c515fa4ddee41ee48a50d760c0ec6
Benedikt-FHDW <benedikt.mohren@edu.fhdw.de>
2024-01-26 12:43:56 +0100

    Sachen

---
+++ Battleship.java
@@ -0,0 +1,40 @@
+public class Battleship {
+
+    public static void main(String[] args) {
+
+    }
+    
+    static int distance(final Coordinate start, final Coordinate end) {
+        return Math.abs(start.column() - end.column()) + Math.abs(start.row() - end.row());
+    }
+    
+    static final int SIZE = 10;
+    
+    static Coordinate getRandomInt (int Random) {
+        return new Coordinate(Utility.getRandomInt(SIZE), Utility.getRandomInt(SIZE));
+    }
+
+    static boolean onOneLine(final Coordinate start, final Coordinate end) {
+        return(start.column() == end.column() || start.row() == end.row());
+    }
+
+    static void showSeperatorLine(){
+        System.out.println("   +-+-+-+-+-+-+-+-+-+-+      +-+-+-+-+-+-+-+-+-+-+");
+    }
+
+    static int getMaxSurroundigColumn(final Coordinate start, final Coordinate end) {
+        return Math.min(Math.max(start.column(), end.column()) + 1, 9);
+    }
+
+    static int getMaxSurroundingRow(final Coordinate start, final Coordinate end) {
+        return Math.min(Math.max(start.row(), end.row()) + 1, 9);
+    }
+
+    static int getMinSurroundingColumn(final Coordinate start, final Coordinate end) {
+        return Math.max(Math.min(start.column(), end.column()) - 1, 0);
+    }
+
+    static int getMinSurroundingRow(final Coordinate start, final Coordinate end) {
+        return Math.max(Math.min(start.row(), end.row()) - 1, 0);
+    }
+}
