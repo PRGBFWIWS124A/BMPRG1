8b838e84d8582f35416f751dee6b577a4824f19b
Benedikt-FHDW <benedikt.mohren@edu.fhdw.de>
2024-05-06 12:30:25 +0200

    gg

--- Aufgabe2.java
+++ Aufgabe2.java
@@ -1,10 +1,17 @@
-public class Aufgabe_2 {
+import java.io.IOException;
+
+public class Aufgabe2{
     public static void main(String[] args) {
         System.out.println("Gib eine Gesamtbreite ein:");
-        String Eingabe = Utility.readStringFromConsole();
+        String Eingabe = "";
+        try{
+        Eingabe = Utility.readStringFromConsole();
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
         int Gesamtbreite = Integer.parseInt(Eingabe);
         double Mittelpunkt = Gesamtbreite / 2;
-        for(int i = 0; i <= Gesamtbreit; i++) {
+        for(int i = 0; i <= Gesamtbreite; i++) {
             for(int j = 0; j <= Gesamtbreite; j++) {
                 if(inHeart(j, i, Mittelpunkt, Mittelpunkt, Gesamtbreite) == true) {
                     System.out.print("#");
@@ -12,13 +19,13 @@ public class Aufgabe_2 {
                     System.out.print(" ");
                 }
             }
-            System.put.println();
+            System.out.println();
         }
     }
 
     static boolean inHeart(double px, double py, double cx, double cy, double g) {
         double b = g/(1+(2/Math.sqrt(2)));
-        return inRhombus(px, py, cx, cy, b) || inCircle(px, py, cx - (b/2), cy - (b/2), b/Math.sprt(2)) || inCircle(px, py, cx + (b/2), cy + (b/2), b/Math.sprt(2));
+        return inRhombus(px, py, cx, cy, b) || inCircle(px, py, cx - (b/2), cy - (b/2), b/Math.sqrt(2)) || inCircle(px, py, cx + (b/2), cy + (b/2), b/Math.sqrt(2));
     }
 
     static boolean inCircle(double px, double py, double cx, double cy, double r) {
@@ -30,7 +37,7 @@ public class Aufgabe_2 {
 
     static boolean inRhombus(double px, double py, double cx, double cy, double b) {
         double a = Math.abs(px - cx);
-        double b = Math.abs(py - cy);
+        double c = Math.abs(py - cy);
         double sum = a + b;
         return sum <= b;
     }
